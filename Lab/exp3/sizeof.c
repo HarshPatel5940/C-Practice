7ce45fe329ec1b3a937c78155d074c3f9a33e326
HARSH PATEL <83941233+HarshPatel5940@users.noreply.github.com>
2022-10-24 18:37:20 +0530

    Typos going rr

--- Lab/exp3/sizeof.c
+++ Lab/exp3/sizeof.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    printf("\nSize the intger datatype x is %d: ", sizeof(int));
-    printf("\nSize the charactor datatype x is %d: ", sizeof(char));
-    printf("\nSize the double datatype x is %d: ", sizeof(double));
-    printf("\nSize the float datatype x is %d: ", sizeof(float));
+    printf("\nSize of the intger datatype x is %d: ", sizeof(int));
+    printf("\nSize of the charactor datatype x is %d: ", sizeof(char));
+    printf("\nSize of the double datatype x is %d: ", sizeof(double));
+    printf("\nSize of the float datatype x is %d: ", sizeof(float));
 
     return 0;
 }
