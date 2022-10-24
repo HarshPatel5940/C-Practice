b1a143f71d74ef1c704d3c41bad9ca6c68f21b03
HARSH PATEL <83941233+HarshPatel5940@users.noreply.github.com>
2022-10-24 18:45:21 +0530

    english is brr

--- Lab/exp3/sizeof.c
+++ Lab/exp3/sizeof.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main() {
-    printf("\nSize of the intger datatype x is %d: ", sizeof(int));
-    printf("\nSize of the charactor datatype x is %d: ", sizeof(char));
-    printf("\nSize of the double datatype x is %d: ", sizeof(double));
-    printf("\nSize of the float datatype x is %d: ", sizeof(float));
+    printf("\nSize of the intger datatype x is: %d", sizeof(int));
+    printf("\nSize of the charactor datatype x is: %d", sizeof(char));
+    printf("\nSize of the double datatype x is: %d", sizeof(double));
+    printf("\nSize of the float datatype x is: %d", sizeof(float));
 
     return 0;
 }
